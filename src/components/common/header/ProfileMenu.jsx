b18664de86d72bfcf0b8f7d49d4607608454be69
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-12-30 13:11:19 +0530

    added logout functionality

--- src/components/common/header/ProfileMenu.jsx
+++ src/components/common/header/ProfileMenu.jsx
@@ -1,9 +1,22 @@
-import { Avatar, Menu } from "antd";
+import { Avatar, Button, Menu } from "antd";
+import { api } from "../../../api";
+import { useNavigate } from "react-router-dom";
 
 function ProfileMenu() {
+  const navigate = useNavigate();
   const onClick = (e) => {
     console.log("click", e);
   };
+
+  function handleLogout() {
+    // remove authentication token from local storage
+    localStorage.removeItem("authToken");
+
+    // remove Authorization from api axios instance
+    delete api.defaults.headers.common["Authorization"];
+
+    navigate("/auth", { replace: true });
+  }
 
   return (
     <Menu
@@ -43,7 +56,7 @@ function ProfileMenu() {
               key: "User Role",
             },
             {
-              label: "Logout",
+              label: <Button onClick={handleLogout}>Logout</Button>,
               key: "Logout",
             },
           ],
