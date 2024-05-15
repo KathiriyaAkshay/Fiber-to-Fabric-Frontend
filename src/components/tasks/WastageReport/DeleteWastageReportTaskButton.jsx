dc02e7cb20808f3e64e77bb321ee18132ca22c1e
keyur-v-crest <keyur.vaghasiya@crestinfosystems.com>
2024-05-15 19:13:38 +0530

    Update wastage report module

--- src/components/tasks/WastageReport/DeleteWastageReportTaskButton.jsx
+++ src/components/tasks/WastageReport/DeleteWastageReportTaskButton.jsx
@@ -50,6 +50,7 @@ const DeleteWastageReportTaskButton = ({ details }) => {
     <div>
       {/* Trigger to open the delete confirmation dialog */}
       <Button
+        danger
         onClick={() => {
           setIsOpenDeleteDialog(true);
         }}
