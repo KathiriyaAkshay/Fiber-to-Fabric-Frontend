169fbe0b16f28e1238994c2eb6d1f126740680d2
RK8998 <kushangrathod5@gmail.com>
2024-10-13 20:57:48 +0530

    integrate rest of actions in material

--- src/pages/material/MillgineReport.jsx
+++ src/pages/material/MillgineReport.jsx
@@ -1,30 +1,22 @@
-import { DeleteOutlined } from "@ant-design/icons";
-import {
-  Button,
-  DatePicker,
-  Flex,
-  Input,
-  Space,
-  Spin,
-  Table,
-  Typography,
-} from "antd";
-import { usePagination } from "../../hooks/usePagination";
-import { useContext, useState } from "react";
+import { DatePicker, Flex, Input, Space, Spin, Table, Typography } from "antd";
+import { useContext, useMemo, useState } from "react";
 import useDebounce from "../../hooks/useDebounce";
 import { getMillgineReportListRequest } from "../../api/requests/material";
 import { GlobalContext } from "../../contexts/GlobalContext";
 import { useQuery } from "@tanstack/react-query";
-
-const data = [{ code: "12", description: "description" }];
+import dayjs from "dayjs";
+import DeleteMillgineReportModal from "../../components/material/DeleteMillgineReport";
 
 const MillgineReport = () => {
+  const { companyId } = useContext(GlobalContext);
+
   const [search, setSearch] = useState("");
+  const [fromDate, setFromDate] = useState(null);
+  const [toDate, setToDate] = useState(null);
 
   const debounceSearch = useDebounce(search, 500);
-
-  const { companyId } = useContext(GlobalContext);
-  const { page, pageSize, onPageChange, onShowSizeChange } = usePagination();
+  const debounceFromDate = useDebounce(fromDate, 500);
+  const debounceToDate = useDebounce(toDate, 500);
 
   const { data: millgineReportData, isLoading } = useQuery({
     queryKey: [
@@ -33,18 +25,18 @@ const MillgineReport = () => {
       "list",
       {
         company_id: companyId,
-        page,
-        pageSize,
         search: debounceSearch,
+        from: debounceFromDate,
+        to: debounceToDate,
       },
     ],
     queryFn: async () => {
       const res = await getMillgineReportListRequest({
         params: {
           company_id: companyId,
-          page,
-          pageSize,
           search: debounceSearch,
+          from: dayjs(debounceFromDate).format("YYYY-MM-DD"),
+          to: dayjs(debounceToDate).format("YYYY-MM-DD"),
         },
       });
       return res.data?.data;
@@ -52,13 +44,15 @@ const MillgineReport = () => {
     enabled: Boolean(companyId),
   });
 
+  const data = useMemo(() => {
+    if (millgineReportData && Object.keys(millgineReportData).length) {
+      return [...millgineReportData.allAudit];
+    } else {
+      return [];
+    }
+  }, [millgineReportData]);
+
   const columns = [
-    // {
-    //   title: "No.",
-    //   dataIndex: "no",
-    //   key: "no",
-    //   render: (_, record, index) => index + 1,
-    // },
     {
       title: "Code",
       dataIndex: "code",
@@ -66,45 +60,79 @@ const MillgineReport = () => {
     },
     {
       title: "Description",
+      dataIndex: "description",
+      key: "description",
+      render: (text) => text || "-",
     },
     {
       title: "Date",
+      dataIndex: "createdAt",
+      key: "createdAt",
+      render: (text) => dayjs(text).format("DD-MM-YYYY"),
     },
     {
       title: "Type",
+      dataIndex: "code",
+      key: "code",
+      render: (text) => text || "-",
     },
     {
       title: "Machine No",
+      dataIndex: "machine_no",
+      key: "machine_no",
+      render: (text) => text || "-",
     },
     {
       title: "Quantity",
+      dataIndex: "quantity",
+      key: "quantity",
+      render: (text) => text || "-",
     },
     {
       title: "Quantity Amount",
+      dataIndex: "quantity_amount",
+      key: "quantity_amount",
+      render: (text) => text || 0,
     },
     {
       title: "Pcs",
+      dataIndex: "pcs",
+      key: "pcs",
+      render: (text) => text || "-",
     },
     {
       title: "Pcs Amount",
+      dataIndex: "amount",
+      key: "amount",
+      render: (text) => text || "-",
     },
     {
       title: "Total Amount",
+      dataIndex: "amount",
+      key: "amount",
+      render: (text) => text || "-",
     },
     {
       title: "User Name",
+      dataIndex: ["user", "username"],
+      key: "username",
+      render: (text) => text || "-",
     },
     {
       title: "Remark",
+      dataIndex: "remark",
+      key: "remark",
+      render: (text) => text || "-",
     },
     {
       title: "Action",
       key: "action",
-      render: () => (
+      render: (details) => (
         <Space>
-          <Button danger>
-            <DeleteOutlined />
-          </Button>
+          <DeleteMillgineReportModal
+            key={"delete_millgine_report_modal"}
+            details={details}
+          />
         </Space>
       ),
     },
@@ -125,21 +153,28 @@ const MillgineReport = () => {
         columns={columns}
         rowKey={"id"}
         scroll={{ y: 330 }}
-        pagination={{
-          total: 0,
-          showSizeChanger: true,
-          onShowSizeChange: onShowSizeChange,
-          onChange: onPageChange,
-        }}
-        summary={() => {
+        pagination={false}
+        summary={(pageData) => {
+          let totalAmountTotal = 0;
+          let totalQuantity = 0;
+          let totalPcs = 0;
+          let totalPcsAmount = 0;
+
+          pageData.forEach((item) => {
+            totalQuantity += +item.quantity || 0;
+            totalPcs += +item.pcs || 0;
+            totalPcsAmount += +item.amount || 0;
+            totalAmountTotal += +item.amount || 0;
+          });
+
           return (
             <Table.Summary.Row>
               <Table.Summary.Cell colSpan={5}>Grand Total</Table.Summary.Cell>
+              <Table.Summary.Cell>{totalQuantity}</Table.Summary.Cell>
               <Table.Summary.Cell>0</Table.Summary.Cell>
-              <Table.Summary.Cell>0</Table.Summary.Cell>
-              <Table.Summary.Cell>0</Table.Summary.Cell>
-              <Table.Summary.Cell>0</Table.Summary.Cell>
-              <Table.Summary.Cell>0</Table.Summary.Cell>
+              <Table.Summary.Cell>{totalPcs}</Table.Summary.Cell>
+              <Table.Summary.Cell>{totalPcsAmount}</Table.Summary.Cell>
+              <Table.Summary.Cell>{totalAmountTotal}</Table.Summary.Cell>
               <Table.Summary.Cell colSpan={3}></Table.Summary.Cell>
             </Table.Summary.Row>
           );
@@ -153,14 +188,19 @@ const MillgineReport = () => {
       <div className="flex items-center justify-end gap-5 mx-3 mb-3">
         <Flex align="center" gap={10}>
           <Typography.Text className="whitespace-nowrap">Date</Typography.Text>
-          <DatePicker />
+          <DatePicker value={fromDate} onChange={(date) => setFromDate(date)} />
         </Flex>
         <Flex align="center" gap={10}>
           <Typography.Text className="whitespace-nowrap">To</Typography.Text>
-          <DatePicker />
+          <DatePicker value={toDate} onChange={(date) => setToDate(date)} />
         </Flex>
         <Flex align="center" gap={10}>
-          <Input placeholder="Search" value={search} onChange={setSearch} />
+          <Input
+            placeholder="Search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            allowClear
+          />
         </Flex>
       </div>
 
