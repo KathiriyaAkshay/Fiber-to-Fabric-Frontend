52148c484470ca328d19f45a245bdd7b404e2ff2
keyur-v-crest <keyur.vaghasiya@crestinfosystems.com>
2024-11-24 17:52:19 +0530

    Update accountant related functionality

--- src/constants/tag.js
+++ src/constants/tag.js
@@ -5,3 +5,6 @@ export const SALE_CHALLAN_INFO_TAG_COLOR = "#f50" ;
 export const TAKA_IP_TAG_COLOR = "magenta" ; 
 export const SAMPLE_CUTTING_TAG_COLOR = "blue" ; 
 export const CUT_TAG_COLOR = "orange" ; 
+export const PURCHASE_YARN_BILL_TAG_COLOR = "cyan" ; 
+export const BEAM_RECEIVE_TAG_COLOR = "#108ee9" ; 
+// export const 
