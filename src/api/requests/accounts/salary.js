e0d4771ce13bd0a115b748eee90aedf3579c6233
RK8998 <kushangrathod5@gmail.com>
2024-12-16 21:05:04 +0530

    integrate work basis salary

--- src/api/requests/accounts/salary.js
+++ src/api/requests/accounts/salary.js
@@ -8,6 +8,18 @@ export function createWorkBasisSalaryRequest({ data, params }) {
   return api.post(`/account/salary-report/work-basis/create`, data, { params });
 }
 
+export function getWorkBasisSalaryListRequest({ params }) {
+  return api.get(`/account/salary/report/generate`, { params });
+}
+
+export function createSalaryReportComponentsRequest({ data, params }) {
+  return api.post(`/account/salary/report/component/create`, data, { params });
+}
+
+export function createPaidSalarySaveRequest({ data, params }) {
+  return api.post(`/account/salary/report/paid/create`, data, { params });
+}
+
 // Advance salary services.
 export function getAdvanceSalaryListRequest({ params }) {
   return api.get(`/account/salary/advanced/list`, { params });
